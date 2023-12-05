981c02dbdb3e99b6652642bb0efe6e157c859c79
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-05 18:42:50 +0900

    feat: validationを注入

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"go-todo-api/repository"
 	"go-todo-api/router"
 	"go-todo-api/usecase"
+	"go-todo-api/validator"
 )
 
 func main(){
 	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	todoValidator := validator.NewTodoValidator()
 	userRepository := repository.NewUserRepository(db)
 	todoRepository := repository.NewTodoRepository(db)
-	userUsecase := usecase.NewUserUsecase(userRepository)
-	todoUsecase := usecase.NewTodoUsecase(todoRepository)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	todoUsecase := usecase.NewTodoUsecase(todoRepository, todoValidator)
 	userController := controller.NewUserController(userUsecase)
 	todoController := controller.NewTodoController(todoUsecase)
 	e := router.NewRouter(userController, todoController)
