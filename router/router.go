b6e60498c44c540b31e16a6d09b8725fc8a9a052
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-05 19:23:07 +0900

    feat: middlewareの追加

--- router/router.go
+++ router/router.go
@@ -2,14 +2,38 @@ package router
 
 import (
 	"go-todo-api/controller"
+	"net/http"
 	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, tc controller.ITodoController) *echo.Echo{
 	e := echo.New()
+	//corsのmiddleware
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		//アクセスを許可するフロントエンドのドメイン
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		//許可するヘッダー一覧
+		//ヘッダー経由でCSRFトークンを受け取る
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,echo.HeaderAccessControlAllowHeaders,echo.HeaderXCSRFToken},
+		AllowMethods: []string{"GET","PUT","POST","DELETE"},
+		//cookieの送受信を可能にする
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		CookiePath: "/",
+		CookieDomain: os.Getenv("API_DOMAIN"),
+		CookieHTTPOnly: true,
+		//postmanなどで動作確認をする際にfalseにする必要になる
+		//自動的にsecure modeがtrueとなるため一時的にコメントアウト
+		// CookieSameSite:http.SameSiteNoneMode ,
+		CookieSameSite: http.SameSiteDefaultMode,
+		//有効期限
+		//CookieMaxAge: 60
+	}))
 	//エンドポイントの追加
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
