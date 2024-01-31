e5c1ce4fb413d8ce5f0558abca9cf3e067e0d828
miyazaki.tara.for.work@gmail.com <miyazaki.tara.for.work@gmail.com>
2024-01-31 13:08:38 +0900

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMzQ3MA/spectral.js?branch_slug=main&token=0d0eb162-f531-42e5-8a07-c346e0dd84a0";
+export default { extends: ruleset };
