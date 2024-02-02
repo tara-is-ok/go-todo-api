17be9154eb9e1172cbfaef7e1eaec9c27e8bb0f9
taraforwork <miyazaki.tara.for.work@gmail.com>
2024-02-02 16:26:17 +0900

    fix: tag.nameのjson名をtitle→name

--- models/tag.go
+++ models/tag.go
@@ -4,7 +4,7 @@ import "time"
 
 type Tag struct {
 	Id        int       `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"title" gorm:"not null"`
+	Name      string    `json:"name" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
