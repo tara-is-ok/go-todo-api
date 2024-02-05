c2f9faf574b1f197f7fedcb347892201024903dc
taraforwork <miyazaki.tara.for.work@gmail.com>
2024-02-05 17:22:48 +0900

    refactor: 中間テーブルは定義不要なため削除

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -15,6 +15,6 @@ func main() {
 	//これらのモデルがデータベース内のテーブルとしてどのカラムを持つか、どのデータ型を使用するかを更新
 	//dbConn.AutoMigrate(models.User{}, models.Todo{})の場合、GORMに対して実際のデータベースのスキーマを変更する指示が通らない。
 	//代わりに、UserとTodoモデルのコピーを作成しそれらのコピーに対してスキーマ変更を行っているが、これらのコピーは元のモデルとは独立していて実際のデータベーステーブルに変更を加えない。結果として、データベースに変更が反映されない
-	dbConn.AutoMigrate(&models.User{}, &models.Todo{}, &models.Tag{}, &models.TodoTag{})
+	dbConn.AutoMigrate(&models.User{}, &models.Todo{}, &models.Tag{})
 
 }
--- models/todoTag.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type TodoTag struct {
-	TodoId int `json:"todo_id" gorm:"primaryKey"`
-	TagId  int `json:"tag_id" gorm:"primaryKey"`
-}
